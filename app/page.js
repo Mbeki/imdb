17493e014930685f898a3d8d558c236df603c8ba
Nixon Mbeki <nwokila@gmail.com>
2024-07-09 20:48:37 +0300

    create the header section

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <section>
-      <div>
-        <h1>Home</h1>
-      </div>
+      {/* <div>
+        <h1 className='text-3xl font-bold'>Home</h1>
+      </div> */}
     </section>
   );
 }
---
+++ components/Header.jsx
@@ -0,0 +1,27 @@
+import MenuItem from './MenuItem';
+import { MdHome } from 'react-icons/md';
+import { BsFillInfoCircleFill } from 'react-icons/bs';
+import Link from 'next/link';
+
+function Header() {
+  return (
+    <header className='mx-2 flex max-w-6xl items-center justify-between py-6 sm:mx-auto'>
+      <div className='flex'>
+        <MenuItem title='HOME' address='/' Icon={MdHome} />
+        <MenuItem title='ABOUT' address='/about' Icon={BsFillInfoCircleFill} />
+      </div>
+      <div className=''>
+        <Link href='/'>
+          <h2 className='text-2xl'>
+            <span className='mr-1 rounded-lg bg-amber-500 px-2 py-1 font-bold'>
+              IMDb
+            </span>
+            <span className='hidden text-xl sm:inline'>Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
