0c38bf2db047d2f825a60791af2760a95f396566
Nixon Mbeki <nwokila@gmail.com>
2024-07-09 23:23:53 +0300

    create the about page

---
+++ app/about/page.jsx
@@ -0,0 +1,35 @@
+function Page() {
+  return (
+    <div className='sp-y-4 mx-auto max-w-6xl'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <p>
+        Risus quis varius quam quisque id diam vel quam elementum. Pharetra
+        convallis posuere morbi leo urna molestie. Posuere urna nec tincidunt
+        praesent semper. Nunc congue nisi vitae suscipit tellus. Magna sit amet
+        purus gravida quis blandit. Maecenas volutpat blandit aliquam etiam erat
+        velit. Neque laoreet suspendisse interdum consectetur libero. Morbi
+        tristique senectus et netus et malesuada. A pellentesque sit amet
+        porttitor eget dolor. Vitae proin sagittis nisl rhoncus mattis rhoncus
+        urna neque viverra. Aliquet nibh praesent tristique magna sit. Vel
+        facilisis volutpat est velit egestas dui id ornare.
+      </p>
+      <p>
+        Eget egestas purus viverra accumsan in nisl nisi scelerisque. Id porta
+        nibh venenatis cras sed felis eget. Dignissim sodales ut eu sem integer
+        vitae justo eget. Nunc aliquet bibendum enim facilisis gravida. Commodo
+        nulla facilisi nullam vehicula.
+      </p>
+    </div>
+  );
+}
+
+export default Page;
