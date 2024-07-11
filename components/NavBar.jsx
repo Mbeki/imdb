4658548b89e62b70ea9a0620784b8e770636b589
Nixon Mbeki <nwokila@gmail.com>
2024-07-11 18:57:38 +0300

    add suspense

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import NavBarItem from './NavBarItem';
 
 function NavBar() {
   return (
-    <div className='flex justify-center bg-amber-100 p-4 lg:text-lg dark:bg-gray-600'>
-      <NavBarItem title='Trending' param='fetchTrending' />
-      <NavBarItem title='Top rated' param='fetchTopRated' />
+    <div className='flex justify-center bg-amber-100 p-4 dark:bg-gray-600 lg:text-lg'>
+      <Suspense fallback='Loading...'>
+        <NavBarItem title='Trending' param='fetchTrending' />
+        <NavBarItem title='Top rated' param='fetchTopRated' />
+      </Suspense>
     </div>
   );
 }
