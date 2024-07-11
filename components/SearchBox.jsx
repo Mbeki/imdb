2c9592a5666730d28009948abcdeabf87a453f72
Nixon Mbeki <nwokila@gmail.com>
2024-07-11 18:34:15 +0300

    add search ability

---
+++ components/SearchBox.jsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+function SearchBox() {
+  const [search, setSearch] = useState('');
+  const router = useRouter();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!search) return;
+    router.push(`/search/${search}`);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className='mx-auto flex max-w-6xl items-center justify-between px-5'
+    >
+      <input
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+        type='text'
+        name=''
+        placeholder='Search keywords...'
+        className='h-14 w-full flex-1 rounded-sm bg-transparent placeholder-gray-500 outline-none'
+      />
+      <button
+        disabled={!search}
+        type='submit'
+        className='text-amber-600 disabled:text-gray-400'
+      >
+        Search
+      </button>
+    </form>
+  );
+}
+
+export default SearchBox;
